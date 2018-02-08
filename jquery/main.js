baca36cf5159aab37629273ab9b649d6e45a3c08
Vishal Parmar <parmar.vish3092@gmail.com>
2018-02-08 12:57:49 +0530

    Progress bar radius change

--- jquery/main.js
+++ jquery/main.js
@@ -2,37 +2,37 @@ $(document).ready(function() {
 	$('#htmlBar').LineProgressbar({
 		percentage : 80,
 		height : '10px',
-		radius : '10px'
+		radius : '0px'
 	});
 	$('#bootBar').LineProgressbar({
 		percentage : 75,
 		height : '10px',
-		radius : '10px'
+		radius : '0px'
 	});
 	$('#jqueryBar').LineProgressbar({
 		percentage : 65,
 		height : '10px',
-		radius : '10px'
+		radius : '0px'
 	});
 	$('#jsonBar').LineProgressbar({
 		percentage : 90,
 		height : '10px',
-		radius : '10px'
+		radius : '0px'
 	});
 	$('#javaCBar').LineProgressbar({
 		percentage : 60,
 		height : '10px',
-		radius : '10px'
+		radius : '0px'
 	});
 	$('#sqlBar').LineProgressbar({
 		percentage : 70,
 		height : '10px',
-		radius : '10px'
+		radius : '0px'
 	});
 	$('#javaScriptBar').LineProgressbar({
 		percentage : 60,
 		height : '10px',
-		radius : '10px'
+		radius : '0px'
 	});
 	
 	var heights = $(".colequal").map(function() {
